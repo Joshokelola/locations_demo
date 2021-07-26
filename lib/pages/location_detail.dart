e76eb76dda5b471eeae5bce2f21428d6dd85aacb
Okelola Joshua <jayokelola341@gmail.com>
2021-07-26 17:24:59 +0100

    created location detail page

--- lib/pages/location_detail.dart
+++ lib/pages/location_detail.dart
@@ -1,42 +1,17 @@
-import 'package:locations/models/location_model.dart';
 import 'package:flutter/material.dart';
 
-class LocationDetail extends StatefulWidget {
-  final Destination destination;
-
-  const LocationDetail({Key? key, required this.destination}) : super(key: key);
+class DestinationScreen extends StatefulWidget {
+  const DestinationScreen({ Key? key }) : super(key: key);
 
   @override
-  _LocationDetailState createState() => _LocationDetailState();
+  _DestinationScreenState createState() => _DestinationScreenState();
 }
 
-class _LocationDetailState extends State<LocationDetail> {
+class _DestinationScreenState extends State<DestinationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.destination.city),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 300,
-              width: double.infinity,
-              child: Image(
-                image: AssetImage(widget.destination.imageUrl),
-              ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Text(
-              widget.destination.city,
-              style: const TextStyle(fontSize: 18.0),
-            ),
-          ],
-        ),
-      ),
+    return Container(
+      
     );
   }
 }
