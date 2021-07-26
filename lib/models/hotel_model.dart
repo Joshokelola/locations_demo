f410496dd3e752f605ee3370e0d0f74644a52ea4
Okelola Joshua <jayokelola341@gmail.com>
2021-07-26 17:24:09 +0100

    updated assets path

--- lib/models/hotel_model.dart
+++ lib/models/hotel_model.dart
@@ -14,19 +14,19 @@ class Hotel {
 
 final List<Hotel> hotels = [
   Hotel(
-    imageUrl: 'assets/sleep_4.jpg',
+    imageUrl: 'assets/hotel0.jpg',
     name: 'Hotel 0',
     address: '404 Great St',
     price: 175,
   ),
   Hotel(
-    imageUrl: 'assets/sleep_6.jpg',
+    imageUrl: 'assets/hotel1.jpg',
     name: 'Hotel 1',
     address: '404 Great St',
     price: 300,
   ),
   Hotel(
-    imageUrl: 'assets/sleep_7.jpg',
+    imageUrl: 'assets/hotel2.jpg',
     name: 'Hotel 2',
     address: '404 Great St',
     price: 240,
