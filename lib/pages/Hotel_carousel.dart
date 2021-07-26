ef7a445023244b2ca5483710aa803186952e2c26
Okelola Joshua <jayokelola341@gmail.com>
2021-07-26 17:24:27 +0100

    Layout and text updates

--- lib/pages/Hotel_carousel.dart
+++ lib/pages/Hotel_carousel.dart
@@ -41,7 +41,7 @@ class HotelCarousel extends StatelessWidget {
                 Hotel hotel = hotels[index];
                 return Container(
                   margin: EdgeInsets.all(10.0),
-                  width: 210,
+                  width: 240,
                   child: Stack(
                     alignment: Alignment.topCenter,
                     children: [
@@ -49,7 +49,7 @@ class HotelCarousel extends StatelessWidget {
                         bottom: 15.0,
                         child: Container(
                           height: 120.0,
-                          width: 200.0,
+                          width: 240.0,
                           decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(10.0),
@@ -58,7 +58,7 @@ class HotelCarousel extends StatelessWidget {
                             padding: const EdgeInsets.all(8.0),
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
                                 Text(
                                   hotel.name,
@@ -72,6 +72,16 @@ class HotelCarousel extends StatelessWidget {
                                   hotel.address,
                                   style: TextStyle(
                                     color: Colors.grey,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 2.0,
+                                ),
+                                Text(
+                                  '\$${hotel.price} / night',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w600,
                                   ),
                                 ),
                               ],
@@ -94,7 +104,7 @@ class HotelCarousel extends StatelessWidget {
                           borderRadius: BorderRadius.circular(20.0),
                           child: Image(
                             height: 180.0,
-                            width: 180.0,
+                            width: 220.0,
                             image: AssetImage(hotel.imageUrl),
                             fit: BoxFit.cover,
                           ),
