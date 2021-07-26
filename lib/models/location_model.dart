0cc68315a2ca1e8a482d0da1614ae3faf60177b0
Okelola Joshua <jayokelola341@gmail.com>
2021-07-26 16:15:43 +0100

    edited location_model

--- lib/models/location_model.dart
+++ lib/models/location_model.dart
@@ -1,14 +1,82 @@
-class Locations {
-  String label;
+import 'activity_model.dart';
+
+class Destination {
   String imageUrl;
+  String city;
+  String country;
+  String description;
+  List<Activity> activities;
 
-  Locations(this.label, this.imageUrl);
-
-  static List<Locations> places = [
-    Locations('France', 'assets/France.jpg'),
-    Locations('Holland', 'assets/holland.jpg'),
-    Locations('New York', 'assets/newYork.jpg'),
-    Locations('Nigeria', 'assets/Nigeria.jpg'),
-    Locations('Cape Town', 'assets/CapeTown.jpg'),
-  ];
+  Destination({
+    required this.imageUrl,
+    required this.city,
+    required this.country,
+    required this.description,
+    required this.activities,
+  });
 }
+
+List<Activity> activities = [
+  Activity(
+    imageUrl: 'assets/stmarksbasilica.jpg',
+    name: 'St. Mark\'s Basilica',
+    type: 'Sightseeing Tour',
+    startTimes: ['9:00 am', '11:00 am'],
+    rating: 5,
+    price: 30,
+  ),
+  Activity(
+    imageUrl: 'assets/gondola.jpg',
+    name: 'Walking Tour and Gonadola Ride',
+    type: 'Sightseeing Tour',
+    startTimes: ['11:00 pm', '1:00 pm'],
+    rating: 4,
+    price: 210,
+  ),
+  Activity(
+    imageUrl: 'assets/murano.jpg',
+    name: 'Murano and Burano Tour',
+    type: 'Sightseeing Tour',
+    startTimes: ['12:30 pm', '2:00 pm'],
+    rating: 3,
+    price: 125,
+  ),
+];
+
+List<Destination> destinations = [
+  Destination(
+    imageUrl: 'assets/newYork.jpg',
+    city: 'Vegas',
+    country: 'America',
+    description: 'Visit Vegas for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+  Destination(
+    imageUrl: 'assets/France.jpg',
+    city: 'Paris',
+    country: 'France',
+    description: 'Visit Paris for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+  Destination(
+    imageUrl: 'assets/holland.jpg',
+    city: 'Holland',
+    country: 'Netherlands',
+    description: 'Visit Holland for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+  Destination(
+    imageUrl: 'assets/Nigeria.jpg',
+    city: 'Lagos',
+    country: 'Nigeria',
+    description: 'Visit Lagos for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+  Destination(
+    imageUrl: 'assets/CapeTown.jpg',
+    city: 'Cape Town',
+    country: 'South Africa',
+    description: 'Visit Cape Town for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+];
