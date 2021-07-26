d4bad753739970342739f66acf1f1ca51ff1bb31
Okelola Joshua <jayokelola341@gmail.com>
2021-07-26 16:16:08 +0100

    still working on this page.

--- lib/pages/location_detail.dart
+++ lib/pages/location_detail.dart
@@ -2,9 +2,9 @@ import 'package:locations/models/location_model.dart';
 import 'package:flutter/material.dart';
 
 class LocationDetail extends StatefulWidget {
-  final Locations location;
+  final Destination destination;
 
-  const LocationDetail({Key? key, required this.location}) : super(key: key);
+  const LocationDetail({Key? key, required this.destination}) : super(key: key);
 
   @override
   _LocationDetailState createState() => _LocationDetailState();
@@ -15,7 +15,7 @@ class _LocationDetailState extends State<LocationDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.location.label),
+        title: Text(widget.destination.city),
       ),
       body: SafeArea(
         child: Column(
@@ -24,14 +24,14 @@ class _LocationDetailState extends State<LocationDetail> {
               height: 300,
               width: double.infinity,
               child: Image(
-                image: AssetImage(widget.location.imageUrl),
+                image: AssetImage(widget.destination.imageUrl),
               ),
             ),
             const SizedBox(
               height: 5,
             ),
             Text(
-              widget.location.label,
+              widget.destination.city,
               style: const TextStyle(fontSize: 18.0),
             ),
           ],
